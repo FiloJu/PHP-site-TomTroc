c392fe08d06d3744e6e2c6e2f2b18140d1d7c458
Filomena Jurgaityte <filomena.jurgaityte@gmail.com>
2026-04-24 10:55:44 +0200

    add router

---
+++ public/index.php
@@ -0,0 +1,72 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use App\BadParameterException;
+use App\Route;
+use App\Router;
+use Controllers\HomeController;
+use Controllers\BookController;
+use Controllers\MessageController;
+use Controllers\AuthController;
+use Controllers\ErrorsController;
+use Middlewares\Authentication;
+
+session_start();
+
+$action = $_GET['action'] ?? '';
+
+try {
+    $router = new Router($action, [
+        new Route([
+            'action' => '',
+            'controller' => HomeController::class,
+            'method' => 'index',
+        ]),
+        new Route([
+            'action' => 'login',
+            'controller' => AuthController::class,
+            'method' => 'login',
+        ]),
+        new Route([
+            'action' => 'books',
+            'controller' => BookController::class,
+            'method' => 'findAll',
+        ]),
+        new Route([
+            'action' => 'book',
+            'controller' => BookController::class,
+            'method' => 'findOne',
+            'parameters' => ['id' => ['format' => '[0-9]+']]
+        ]),
+        new Route([
+            'action' => 'create-message',
+            'controller' => MessageController::class,
+            'method' => 'create',
+            'middlewares' => [
+                Authentication::class => 'checkAuth'
+            ]
+        ])
+    ]);
+    $router->errorRoutes([
+        new Route([
+            'action' => '403', 
+            'controller' => ErrorsController::class, 
+            'method' => 'error403'
+        ]),
+        new Route([
+            'action' => '404', 
+            'controller' => ErrorsController::class, 
+            'method' => 'error404'
+        ]),
+        new Route([
+            'action' => '400', 
+            'controller' => ErrorsController::class, 
+            'method' => 'error400'
+        ]),
+    ]);
+    $router->route();
+} catch (InvalidArgumentException $e) {
+    echo $e->getMessage();
+} catch (BadParameterException $e) {
+    return header('location: index.php?action=400');
+}
---
+++ src/app/Router.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App;
+
+use InvalidArgumentException;
+class Router
+{
+    private array $routes = [];
+    private string $action = '';
+    private array $errorRoutes = [];
+
+    public function __construct(string $action, array $routes)
+    {
+        $this->action = $action;
+        $this->routes = $routes;
+    }
+
+    public function errorRoutes(array $errorRoutes)
+    {
+        $this->errorRoutes = $errorRoutes;
+    }
+    public function route()
+    {
+        foreach ($this->routes as $route) {
+            if ($route->action() === $this->action and $this->requestIs($route->verb())) {
+                if ($route->hasParameters() and !$this->parametersMatch($route->parameters())) {
+                    throw new BadParameterException('Les paramètres de la route ne correspondent pas à ceux de la requête');
+                }
+                if ($route->hasMiddlewares()) {
+                    $this->callMiddlewares($route->middlewares());
+                }
+                return $this->callController($route->controller(), $route->method(), $route->parameters());
+            }
+        }
+        foreach ($this->errorRoutes as $route) {
+            if ($route->action() === $this->action and $this->requestIs('GET')) {
+                http_response_code($this->action);
+                return $this->callController($route->controller(), $route->method());
+            }
+        }
+        return header('location: index.php?action=404');
+    }
+
+    private function parametersMatch(array $parameters)
+    {
+        foreach ($parameters as $parameter => $constraints) {
+            if (is_string($constraints)) {
+                $parameter = $constraints;
+            }
+            if ($this->parameterIsRequired($constraints) and empty($_GET[$parameter])) {
+                return false;
+            }
+            if (!$this->parameterHasGoodFormat($parameter, $constraints)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private function callMiddlewares(array $middlewares)
+    {
+        foreach ($middlewares as $middleware => $method) {
+            $middleware = new $middleware();
+            $middleware->$method();
+        }
+    }
+
+    private function callController(string $controller, string $method, array $parameters = [])
+    {
+        if (!method_exists($controller, $method)) {
+            throw new InvalidArgumentException('Impossible d\'appeler la méthode du contrôleur');
+        }
+        $controller = new $controller();
+        
+        // Extraire les paramètres de la requête GET
+        $requestParams = [];
+        foreach ($parameters as $param => $constraints) {
+            if (is_string($param)) {
+                $requestParams[$param] = $_GET[$param] ?? null;
+            }
+        }
+        
+        // Appeler la méthode avec les paramètres si nécessaire
+        if (!empty($requestParams)) {
+            $controller->$method(...array_values($requestParams));
+        } else {
+            $controller->$method();
+        }
+    }
+
+    private function requestIs(string $verb): bool
+    {
+        return $_SERVER['REQUEST_METHOD'] === $verb;
+    }
+
+    private function parameterIsRequired(array|string $constraints): bool
+    {
+        if (!is_array($constraints)) {
+            return true;
+        }
+        // Le paramètre est requis s'il n'y a pas required OU que required est true
+        return !isset($constraints['required']) || $constraints['required'];
+    }
+
+    private function parameterHasGoodFormat(string $parameter, array|string $constraints): bool
+    {
+        if (is_array($constraints) and isset($constraints['format']) and !empty($_GET[$parameter])) {
+            return preg_match('/^' . $constraints['format'] . '$/', $_GET[$parameter]);
+        }
+        return true;
+    }
+}

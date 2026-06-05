a594f9d06a5ad2b3b409a289e17d864bac825ced
Filomena Jurgaityte <filomena.jurgaityte@gmail.com>
2026-06-05 11:16:56 +0200

    refactor database class

--- src/models/Database.php
+++ src/models/Database.php
@@ -2,21 +2,46 @@
 
 namespace Models;
 use PDO;
+use PDOStatement;
 require_once '../config/database.php';
 class Database {
-    private static $instance = null;
 
 // Singleton pattern to ensure only one connection is created - one and only one instance of PDO is used throughout the application
-// TODO :Voir pourquoi on ne passe pas dedans
-    public static function connect() {
+    private static $instance = null;
+
+    private $db;
+
+    private function __construct() {
+        $this->db = new PDO(
+            'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
+            DB_USER,
+            DB_PASS
+        );
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }
+
+    public static function getInstance(): Database
+    {
         if (self::$instance === null) {
-            self::$instance = new PDO(
-                'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
-                DB_USER,
-                DB_PASS
-            );
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$instance = new Database();
         }
         return self::$instance;
+    }
+
+    public static function connect(): PDO
+    {
+        return self::getInstance()->db;
+    }
+
+    public function query(string $sql, array $params = []): PDOStatement
+    {
+        if (empty($params)) {
+            return $this->db->query($sql);
+        } else {
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute($params);
+            return $stmt;
+        }
     }
 }

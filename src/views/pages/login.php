33f60ab2454caf7e6e7863e83e2d6ae823ed2c33
Filomena Jurgaityte <filomena.jurgaityte@gmail.com>
2026-06-26 17:56:59 +0200

    add image to login and register page

--- src/views/pages/login.php
+++ src/views/pages/login.php
@@ -1,23 +1,27 @@
-<section class="auth">
-    <div class="auth-left">
-        <h1 class="sign-title">Connexion</h1>
+<main class="main-auth">
+    <div class="auth">
+        <div class="auth-left">
+            <h1 class="sign-title">Connexion</h1>
 
-        <form method="POST" action="index.php?action=login">
-            <div class="field">
-                <label for="email">Adresse email</label>
-                <input type="email" id="email" name="email" required>
+            <form method="POST" action="index.php?action=login" class="sign-form">
+
+                <div class="field">
+                    <label for="email">Adresse email</label>
+                    <input type="email" name="email" id="email" required>
+                </div>
+
+                <div class="field">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" name="password" id="password" required>
+                </div>
+
+                <button type="submit" class="sign-submit-btn">Se connecter</button>
+            </form>
+
+            <div class="sign-ask">
+                Pas de compte ? <a href="index.php?action=register">Inscrivez-vous</a>
             </div>
-            <div class="field">
-                <label for="password">Mot de passe</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <button type="submit" class="sign-submit-btn">Se connecter</button>
-        </form>
+        </div>
 
-        <p class="sign-ask">Pas de compte ? <a href="index.php?action=register">Inscrivez-vous</a></p>
+        <img src="img/books/image_inscription.jpg" class="auth-img" alt="Bibliothèque">
     </div>
-
-    <div class="auth-img">
-        <img src="img/min/livres.svg" alt="Illustration connexion">
-    </div>
-</section>
--- src/views/pages/register.php
+++ src/views/pages/register.php
@@ -1,27 +1,32 @@
-<section class="auth">
-    <div class="auth-left">
-        <h1 class="sign-title">Inscription</h1>
+<main class="main-auth">
+    <div class="auth">
+        <div class="auth-left">
+            <h1 class="sign-title">Inscription</h1>
 
-        <form method="POST" action="index.php?action=createUser">
-            <div class="field">
-                <label for="username">Pseudo</label>
-                <input type="text" id="username" name="username" required>
-            </div>
-            <div class="field">
-                <label for="email">Adresse email</label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div class="field">
-                <label for="password">Mot de passe</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <button type="submit" class="sign-submit-btn">S'inscrire</button>
-        </form>
+            <form method="POST" action="index.php?action=createUser" class="sign-form">
+                <div class="field">
+                    <label for="pseudo">Pseudo</label>
+                    <input type="text" name="pseudo" id="pseudo" required>
+                </div>
 
-        <p class="sign-ask">Déjà inscrit ? <a href="index.php?action=login">Connectez-vous</a></p>
+                <div class="field">
+                    <label for="email">Adresse email</label>
+                    <input type="email" name="email" id="email" required>
+                </div>
+
+                <div class="field">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" name="password" id="password" required>
+                </div>
+
+                <button type="submit" class="sign-submit-btn">S'inscrire</button>
+            </form>
+
+            <div class="sign-ask">
+                Déjà inscrit ? <a href="index.php?action=login">Connectez-vous</a>
+            </div>
+        </div>
+
+        <img src="img/books/image_inscription.jpg" class="auth-img" alt="Bibliothèque">
     </div>
-
-    <div class="auth-img">
-        <img src="img/min/livres.svg" alt="Illustration inscription">
-    </div>
-</section>
+</main>

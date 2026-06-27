e7cfd0065e0e78e5ef3ab136f27c35aeadd93b44
Filomena Jurgaityte <filomena.jurgaityte@gmail.com>
2026-06-27 17:26:26 +0200

    ajust home page

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,18 +1,20 @@
 <section class="banner">
-    <div class="banner-content">
+    <div class="banner-inner">
+        <div class="banner-content">
         <h1 class="title">Rejoignez nos lecteurs passionnés</h1>
         <p class="text">
             Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture.
             Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.
         </p>
         <a href="index.php?action=books" class="btn">Découvrir</a>
-    </div>
+        </div>
 
-    <div class="banner-image">
+        <div class="banner-image">
         <img src="img/books/book_hope.jpg" alt="author" class="image">
 
         <div class="card">
             <span class="name">xxx</span>
+        </div>
         </div>
     </div>
 </section>
@@ -53,18 +55,20 @@
 </section>
 
 <section class="steps">
-    <h2 class="section-title">Comment ça marche ?</h2>
-    <p class="subtitle">Échanger des livres avec TomTroc c'est simple et<br> amusant ! Suivez ces étapes pour commencer :</p>
+    <div class="steps-inner">
+        <h2 class="section-title">Comment ça marche ?</h2>
+        <p class="subtitle">Échanger des livres avec TomTroc c'est simple et<br> amusant ! Suivez ces étapes pour commencer :</p>
 
-    <div class="steps-list">
-        <div class="step">Inscrivez-vous gratuitement sur notre plateforme.</div>
-        <div class="step">Ajoutez les livres que vous souhaitez échanger à votre profil.</div>
-        <div class="step">Parcourez les livres disponibles chez d'autres membres.</div>
-        <div class="step">Proposez un échange et discutez avec d'autres passionnés de lecture.</div>
-    </div>
+        <div class="steps-list">
+            <div class="step">Inscrivez-vous gratuitement sur notre plateforme.</div>
+            <div class="step">Ajoutez les livres que vous souhaitez échanger à votre profil.</div>
+            <div class="step">Parcourez les livres disponibles chez d'autres membres.</div>
+            <div class="step">Proposez un échange et discutez avec d'autres passionnés de lecture.</div>
+        </div>
 
-    <div class="center-btn">
-        <a href="index.php?action=books" class="btn-outline">Voir tous les livres</a>
+        <div class="center-btn">
+            <a href="index.php?action=books" class="btn-outline">Voir tous les livres</a>
+        </div>
     </div>
 </section>
 
